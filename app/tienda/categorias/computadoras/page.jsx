f1a613ad73815ffbf5c7fe547187fdab23332c8a
Andres Bravo <bravoandres1706@gmail.com>
2025-05-28 09:58:52 -0400

    first commit

---
+++ app/tienda/categorias/computadoras/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ComputadorasPage = () => {
+  return (
+    <div>
+        <h1>Computadoras</h1>
+    </div>
+  )
+}
+
+export default ComputadorasPage
---
+++ app/tienda/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Tienda = () => {
+    return (
+        <div> 
+            <h1>Tienda</h1>
+            <ul>
+                <li>
+                    Product1
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Tienda
